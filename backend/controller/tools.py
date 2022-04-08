f8c3a14ad7002db5acd8f3ccebc690c224c3953f
vdsbenoit <vdsbenoit@gmail.com>
2022-04-08 21:35:25 +0200

    Do not parse empty lines from csv files

--- backend/controller/tools.py
+++ backend/controller/tools.py
@@ -5,5 +5,5 @@ import settings
 def parse_csv(filename: str):
     with open(filename, mode="r", encoding="utf-8-sig") as csvfile:
         reader = csv.DictReader(csvfile, delimiter=settings.csv.delimiter)
-        data = [row for row in reader]
+        data = [row for row in reader if row[list(row.keys())[0]]]
     return data
