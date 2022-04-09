1e31bd8a8203768295efaf1c6a6a0367cf7ff86f
vdsbenoit <vdsbenoit@gmail.com>
2022-04-09 14:15:22 +0200

    Update settings

--- backend/sandbox.py
+++ backend/sandbox.py
@@ -8,6 +8,6 @@ db = main.init_db()
 
 
 CSV_PATH = "data/repartition2022.csv"
-# controller.initialization.create_new_db(db, 17, CSV_PATH)
+controller.initialization.create_new_db(db, 17, CSV_PATH)
 
-generate_badges(db, "badges.pdf")
+# generate_badges(db, "badges.pdf")
